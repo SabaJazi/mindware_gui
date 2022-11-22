cf8a5c035031d07b49e6b721d89cbd15aac921d2
Yousefian Jazi <Saba.YousefianJazi@unt.edu>
2022-11-22 15:55:43 -0600

    working_sample

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
-# import tkinter as tk
+import tkinter as tk
 from tkinter import *
-# from tkinter import ttk
+from tkinter import ttk
 from tkinter.ttk import *
 # from pandas import DataFrame
 # import matplotlib.pyplot as plt
@@ -16,18 +16,38 @@ def func1():
 root = Tk()             
  
 # Open window having dimension 100x100
-root.geometry('200x150')
+root.geometry('450x300')
+
+# the label for dataset
+data_path = Label(root,
+text = "data path").place(x = 40, y = 60) 
+     
+data_path_input_area = Entry(root,
+width = 30).place(x = 110, y = 60) 
+# ------------------------------------------------
+
+load_button = Button(root,
+text = "Load").place(x = 40, y = 130)
+
+exit_button = Button(root,
+text = "Exit").place(x = 140, y = 130)
+# -------------------------------------------------------
+  
+# label
+ttk.Label(root, text = "Select model type:",
+          font = ("Times New Roman", 10)).grid(column = 0,
+          row = 9, padx = 10, pady = 25)
+  
+# Combobox creation
+n = tk.StringVar()
+monthchoosen = ttk.Combobox(root, width = 27, textvariable = n)
+  
+# Adding combobox drop down list
+monthchoosen['values'] = (' Classifyer', ' Bagging' )
+  
+monthchoosen.grid(column = 1, row = 9)
+monthchoosen.current()
 
 
- 
-# Create a Button
-btn1 = Button(root, text = 'Select dataset !',
-                          command = func1())
-btn2 = Button(root, text = 'Exit !', 
-                          command = root.destroy)
- 
-# Set the position of button on the top of window. 
-btn1.pack(side = 'top')  
-btn2.pack(side = 'bottom')   
- 
 root.mainloop()
+print("test")
